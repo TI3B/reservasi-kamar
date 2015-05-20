b52d231a1ad20eeedb7a52662837b25f4e19ef87
indahfitria <fitria.indah12@gmail.com>
2015-05-20 08:46:21 +0700

    insert grafik

---
+++ application/controllers/dashboard.php
@@ -0,0 +1,77 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+//error_reporting(0);
+class Dashboard extends CI_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->model('booking_m');
+		$this->load->model('kamar_m');
+		$this->load->model('tipe_kamar_m');
+	}
+
+	public function index()
+	{
+		//$tipe_kamar = $this->tipe_kamar_m->selectAll();
+		$data['data'] = "<script type=\"text/javascript\">
+    var chart1; // globally available
+	$(document).ready(function() {
+      chart1 = new Highcharts.Chart({
+         chart: {
+            renderTo: 'grafik',
+            type: 'column'
+         },   
+         title: {
+            text: 'Grafik Booking '
+         },
+         xAxis: {
+            categories: ['nama_tipe']
+         },
+         yAxis: {
+            title: {
+               text: 'Jumlah'
+            }
+         },
+              series:             
+            [";
+
+		$tipe_kamar = $this->tipe_kamar_m->selectAll();
+		foreach ($tipe_kamar as $row)
+		{
+			$jumlah = 0;
+			$kd_tipe_kamar = $row->kd_tipe_kamar;
+			$nama_tipe_kamar = $row->nama_tipe_kamar;
+			$kamar = $this->kamar_m->selectByField('kd_tipe_kamar', $kd_tipe_kamar);
+			foreach ($kamar as $row)
+			{
+				$kd_kamar = $row->kd_kamar;
+				$booking = $this->booking_m->selectAll();
+				foreach ($booking as $row)
+				{
+					if ($kd_kamar == $row->kd_kamar)
+					{
+						$jumlah++;
+					}
+				}
+				// $booking = $this->booking_m->jumlahTipeKamar('kd_kamar', $kd_kamar);
+				// $jumlah = $jumlah+$booking;
+			}
+			$data['data'] .= "
+				{
+					name: '$nama_tipe_kamar',
+					data: [$jumlah]
+				},
+				";
+		}
+		$data['data'] .= " ]
+      });
+   });  
+</script>";
+
+		$d['title'] = 'Dashboard';
+		$this->load->view('header_v', $d);
+		$this->load->view('dashboard_v', $data);
+		$this->load->view('footer_v');		
+	}
+
+}
---
+++ application/views/dashboard_v.php
@@ -0,0 +1,22 @@
+<?php echo $data; ?>
+            <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Dashboard
+                        </h1>
+                        <ol class="breadcrumb">
+                            <li class="active">
+                                <i class="fa fa-dashboard"></i>  <a href="">Dashboard</a>
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+                <!-- /.row -->
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div id='grafik'></div>
+                    </div>
+                </div>
+                <!-- /.row -->
