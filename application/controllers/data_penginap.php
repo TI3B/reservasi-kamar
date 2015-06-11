68d21d255d85d760f5c100a022faeb065bcbfbde
FaristyaDaraKejora <faristyakejora@gmail.com>
2015-06-11 18:13:59 +0700

    edit data_penginap.php ver1.2

--- application/controllers/data_penginap.php
+++ application/controllers/data_penginap.php
@@ -21,11 +21,24 @@ class Data_penginap extends CI_Controller {
 	{
 		if ($this->input->post('add'))
 		{
+			$lokasi_file    = $_FILES['fupload']['tmp_name'];
+				$tipe_file      = $_FILES['fupload']['type'];
+				$nama_file      = $_FILES['fupload']['name'];
+				$acak           = rand(1,99999);
+				$nama_file_ktp = $acak.$nama_file ;
 
 			$data = array('kd_data_penginap' => $this->input->post('kd_data_penginap')
+						 ,'NIK' => $this->input->post('NIK')
 						 ,'nama_penginap' => $this->input->post('nama_penginap')
 						 ,'alamat' => $this->input->post('alamat')
-						 ,'no_telp' => $this->input->post('no_telp');
+						 ,'no_telp' => $this->input->post('no_telp')
+						 ,'foto_ktp' => $nama_file_ktp);
+
+			$vdir_upload = "asset/images/";
+						$vfile_upload = $vdir_upload.$nama_file_ktp;
+						//Simpan file dalam ukuran sebenarnya
+						move_uploaded_file($lokasi_file, $vfile_upload);
+
 
 			$this->data_penginap_m->insert($data);
 			echo "<script>alert('Proses Input Data Berhasil :)'); window.location = '".base_url()."data_penginap'</script>";
@@ -41,12 +54,24 @@ class Data_penginap extends CI_Controller {
 	{
 		if ($this->input->post('edit'))
 		{
+			$lokasi_file    = $_FILES['fupload']['tmp_name'];
+				$tipe_file      = $_FILES['fupload']['type'];
+				$nama_file      = $_FILES['fupload']['name'];
+				$acak           = rand(1,99999);
+				$nama_file_ktp = $acak.$nama_file ;
 			
-			$data = array('nama_penginap' => $this->input->post('nama_penginap')
+			$data = array('NIK' => $this->input->post('NIK')
+						 ,'nama_penginap' => $this->input->post('nama_penginap')
 						 ,'alamat' => $this->input->post('alamat')
-						 ,'no_telp' => $this->input->post('no_telp');
-						
-			
+						 ,'no_telp' => $this->input->post('no_telp')
+						 ,'foto_ktp' => $nama_file_ktp);
+
+			$vdir_upload = "asset/images/";
+						$vfile_upload = $vdir_upload.$nama_file_ktp;
+						//Simpan file dalam ukuran sebenarnya
+						move_uploaded_file($lokasi_file, $vfile_upload);
+
+//mencoba mengubah data
 			$this->data_penginap_m->update($data,$id);
 			echo "<script>alert('Proses Edit Data Berhasil :)'); window.location = '".base_url()."data_penginap'</script>";
 		}
